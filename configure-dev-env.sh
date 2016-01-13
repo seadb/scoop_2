2b2ea173f6868b1a4793fef07a54412988586e3e
Chelsea Bridson <bridsonc@msu.edu>
2016-01-12 21:47:51 -0500

    added script for setting up dev env"

---
+++ configure-dev-env.sh
@@ -0,0 +1,12 @@
+echo -e "Configuring dev environment"
+ssk-keygen -t rsa -b 4096 -f ~/.ssh/github 
+eval `ssh-agent`
+ssh-add ~/.ssh/github
+echo -e "Copy and paste the following into your github keys:\n"
+cat ~/.ssh/github.pub
+echo -e "\nThen press any key"
+read input
+echo -e "Testing github authentication"
+ssh -T git@github.com
+git clone git@github.com:seadb/.config.git ~/.config
+(cd ~/.config; ./install.sh)
