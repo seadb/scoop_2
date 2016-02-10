6b5d80e2daa2b35683e8e2c8f263e0062c443af4
Chelsea Bridson <bridsonc@msu.edu>
2016-02-10 22:35:53 +0000

    Added installation script for macosx, started implementing user authentication new file:   install-dependencies-macosx.sh modified:   scoop/Gemfile modified:   scoop/Gemfile.lock new file:   scoop/app/assets/javascripts/sessions.js.coffee new file:   scoop/app/assets/javascripts/users.js.coffee new file:   scoop/app/assets/stylesheets/sessions.css.scss new file:   scoop/app/assets/stylesheets/users.css.scss new file:   scoop/app/controllers/sessions_controller.rb new file:   scoop/app/controllers/users_controller.rb new file:   scoop/app/helpers/sessions_helper.rb new file:   scoop/app/helpers/users_helper.rb new file:   scoop/app/models/user.rb new file:   scoop/app/views/sessions/new.html.erb new file:   scoop/app/views/users/new.html.erb modified:   scoop/config/routes.rb new file:   scoop/db/migrate/20160131214713_create_users.rb new file:   scoop/db/schema.rb new file:   scoop/test/controllers/sessions_controller_test.rb new file:   scoop/test/controllers/users_controller_test.rb new file:   scoop/test/fixtures/users.yml new file:   scoop/test/helpers/sessions_helper_test.rb new file:   scoop/test/helpers/users_helper_test.rb new file:   scoop/test/models/user_test.rb

--- scoop/config/routes.rb
+++ scoop/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
